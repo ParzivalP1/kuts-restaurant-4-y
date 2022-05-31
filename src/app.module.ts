a07d09e7b7a0eb2890a0bb5127f9bd17f3e65bb3
Kuts Nikita <nikitakuts@mail.ru>
2022-05-31 15:26:40 +0300

    new labs added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { PostsModule } from './posts/posts.module';
 import { ProfileModule } from './profile/profile.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PostsModule, ProfileModule, UsersModule],
+  imports: [ProfileModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
